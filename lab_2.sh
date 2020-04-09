2c1b7192466e1ae5a46ed93e882f60995e263331
Roksolana Trofymchuk <36045973+donuthack@users.noreply.github.com>
2020-04-09 18:50:23 +0000

    Add bash file for 2 lab task

---
+++ lab_2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /etc/
+cd /home/
+cd lab_tasks/2/example_folder
+cd ../
+ls ~/lab_tasks/
+
+cd /home/ && pwd
+cd /home/lab && pwd
+cd ~/lab_tasks/2/example_folder && cd ../
+cd ~/lab_tasks/2 && ls
+
+cd ~/lab_tasks/2/example_dir && ls -a
+cd && ls
