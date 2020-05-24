aa14d11c00d9713f5e5e55df59228ca6123dc4e5
Roksolana Trofymchuk <36045973+donuthack@users.noreply.github.com>
2020-05-24 21:24:59 +0300

    Update lab6.sh

--- 6/lab6.sh
+++ 6/lab6.sh
@@ -6,30 +6,30 @@
 
 
 
-#touch ~/Desktop/6/6.log
+touch ~/Desktop/6/6.log
 
-#echo "ROKSOLANA" > ~/Desktop/6/6.log
+echo "ROKSOLANA" > ~/Desktop/6/6.log
 
-#date >> ~/Desktop/6/6.log
+date >> ~/Desktop/6/6.log
 
-#last -n 1 >> ~/Desktop/6/6.log
+last -n 1 >> ~/Desktop/6/6.log
 
-#cat ~/i_dont_exist.txt
+cat ~/i_dont_exist.txt
 
-#cat ~/i_dont_exist.txt 2> /dev/null
+cat ~/i_dont_exist.txt 2> /dev/null
 
-#cat ~/Desktop/6/6.log 2> /dev/null
+cat ~/Desktop/6/6.log 2> /dev/null
 
 
 ### Middle Level ###
 
 
-#ping i.ua >> ~/Desktop/6/6.log
+ping i.ua >> ~/Desktop/6/6.log
 
-#touch ~/Desktop/6/6.errors
-#ping a.ua >> ~/Desktop/6/6.errors
+touch ~/Desktop/6/6.errors
+ping a.ua >> ~/Desktop/6/6.errors
 
-#echo "This is random number: $RANDOM" >> ~/Desktop/6/6.log
+echo "This is random number: $RANDOM" >> ~/Desktop/6/6.log
 
 # for exit from VIM: press ESC button, than press SHIFT+: and write Q if you want to exit without saving, or write WQ if you want to save changes and exit
 
@@ -38,9 +38,9 @@
 
 ### Senior Level ###
 
-#read -p "Enter your site for ping: " site
-#ping -c 10 $site 1> ~/Desktop/6/6.log
-#ping -c 10 $site 2> ~/Desktop/6/6.errors
+read -p "Enter your site for ping: " site
+ping -c 10 $site 1> ~/Desktop/6/6.log
+ping -c 10 $site 2> ~/Desktop/6/6.errors
 
 read -p "Enter your site for ping: " site
 ping -c 10 $site >> res.txt || ping -c 10 $site 2>> errors.txt
